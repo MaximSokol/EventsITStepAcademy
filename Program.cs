b82cdd30f06a502ea9b0ed243312001fa0d336ff
SokolMaxim <maximsokol2288@gmail.com>
2022-05-04 20:11:01 +0300

    FCommit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsITStepAcademy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.PropertyChanged += Student_PropertyChanged;
+            student.Name = "Alex";
+
+        }
+
+        private static void Student_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            Console.WriteLine($"The name was changed!\n");
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace EventsITStepAcademy
+{
+    class Student : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        //----------------------------------------------------------
+
+        private string surname;
+
+        public string Surname
+        {
+            get { return surname; }
+            set 
+            { 
+                surname = value;
+                OnProperyChanged("Surname");
+            }
+        }
+        //---------------------------------------
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set 
+            { 
+                name = value;
+                OnProperyChanged("Name");
+            }
+        }
+        //---------------------------------------
+
+        private string patronymic;
+
+        public string Patronymic
+        {
+            get { return patronymic; }
+            set 
+            { 
+                patronymic = value;
+                OnProperyChanged("Patronymic");
+            }
+        }
+        //---------------------------------------
+
+        private void OnProperyChanged(string propertyName)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, null);
+            }
+        }
+        //--------------------------------------------------------
+    }
+}
